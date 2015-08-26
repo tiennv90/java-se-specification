1895c9113910c39b5b8a078e3e1f3e1baf484091
tiennv <tiennv90@gmail.com>
2015-08-27 00:05:03 +0700

    Iterable

---
+++ src/com/java/spec/tiennv/java/lang/UsingIterable.java
@@ -0,0 +1,12 @@
+package com.java.spec.tiennv.java.lang;
+
+import java.util.Iterator;
+
+public class UsingIterable implements Iterable<String> {
+
+	@Override
+	public Iterator<String> iterator() {
+		return null;
+	}
+
+}
