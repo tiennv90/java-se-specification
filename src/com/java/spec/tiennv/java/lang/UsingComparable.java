8f14068b3ea7a29b2d867c461f15551322d4768b
tiennv <tiennv90@gmail.com>
2015-08-26 23:34:20 +0700

    update Comparable use cases

--- src/com/java/spec/tiennv/java/lang/UsingComparable.java
+++ src/com/java/spec/tiennv/java/lang/UsingComparable.java
@@ -3,6 +3,9 @@ package com.java.spec.tiennv.java.lang;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 
 public class UsingComparable {
@@ -19,6 +22,15 @@ public class UsingComparable {
 		
 		for (IntegerComparableImpl number : list) {
 			System.out.println(number.number);
+		}
+		
+		Map<IntegerComparableImpl, String> sortedMap = new TreeMap<>();
+		sortedMap.put(n1, "value 1");
+		sortedMap.put(n3, "value 3");
+		sortedMap.put(n2, "value 2");
+		
+		for (Entry<IntegerComparableImpl, String> entry : sortedMap.entrySet()) {
+			System.out.println(entry.getValue());
 		}
 	}
 
